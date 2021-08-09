a53ee82b1dcec823cfbeeeb8255be01f1f211b16
delongamateo <delongamateo@gmail.com>
2021-08-09 16:55:22 +0200

    displaying google maps

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,7 +6,7 @@ import useStyles from "./styles"
 
 function Header() {
     const classes = useStyles();
-    
+
     return (
         <AppBar>
             <Toolbar className={classes.toolbar}>
@@ -17,14 +17,14 @@ function Header() {
                     <Typography variant="h6" className={classes.title}>
                         Explore new places
                     </Typography>
-                    <Autocomplete>
+                    {/* <Autocomplete> */}
                         <div className={classes.search}>
                             <div className={classes.searchIcon}>
                                 <SearchIcon />
                             </div>
                             <InputBase placeholder="Search..." classes={{root: classes.inputRoot, input: classes.inputInput}} />
                         </div>
-                    </Autocomplete>
+                   {/*  </Autocomplete> */}
                 </Box>
             </Toolbar>
         </AppBar>
